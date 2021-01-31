1d35c16578e9a763393411da38fe97edc57ee61a
PlanetTeamSpeakk <PlanetTeamSpeakk@users.noreply.github.com>
2021-01-31 23:29:06 +0100

    Movable backlight
    It follows your cursor in an oval-like fashion.
    Also the ability to increase its intensity and angle by scrolling.

--- data/script.js
+++ data/script.js
@@ -24,8 +24,8 @@ const intensity = parseFloat(getQueryVariable("intensity") || "1");
 const flag = getQueryVariable("flag") || "gay";
 const colours = getQueryVariable("colour", true) || getQueryVariable("color", true) || flags[flag] || flags["gay"];
 const ctx = document.createElement("canvas").getContext("2d");
-ctx.canvas.width = window.innerWidth;
-ctx.canvas.height = window.innerHeight;
+ctx.canvas.width = 1920;
+ctx.canvas.height = 1080;
 const rowHeight = ctx.canvas.height / colours.length;
 for (var i = 0; i < colours.length; i++) {
   ctx.fillStyle = colours[i];
@@ -34,19 +34,21 @@ for (var i = 0; i < colours.length; i++) {
 
 var started = false, paused = false;
 var elapsedTime = -1;
+var centerX, centerY;
 const loader = new THREE.TextureLoader();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 10;
 const renderer = new THREE.WebGLRenderer({antialias: true});
 const plane = new THREE.PlaneGeometry(5, 3, 50, 30);
 const material = new THREE.MeshPhongMaterial({map: loader.load(ctx.canvas.toDataURL())});
 const flagMesh = new THREE.Mesh(plane, material);
 scene.add(flagMesh);
 
-const leftLight = new THREE.SpotLight(0xFFFFFF, 2);
-leftLight.position.set(-5, 0, 1.5);
-scene.add(leftLight);
+const backlight = new THREE.SpotLight(0xFFFFFF, 2);
+backlight.position.set(-5, 0, 1.5);
+backlight.penumbra = 1;
+
 const clock = new THREE.Clock({autostart: false});
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -62,7 +64,7 @@ window.addEventListener('resize', function() {
   setup();
 });
 
-document.addEventListener('visibilitychange', function(ev) {
+document.addEventListener('visibilitychange', function (e) {
   if (document.visibilityState == "visible") {
     if (started) {
       clock.start();
@@ -76,21 +78,87 @@ document.addEventListener('visibilitychange', function(ev) {
   }
 });
 
+document.addEventListener('mousemove', function (e) {
+  var angle = Math.atan2(e.clientY - centerY, e.clientX - centerX) + Math.PI;
+  backlight.position.set(-Math.cos(angle) * 5, Math.sin(angle) * 3.5, 1.5);
+});
+
+document.addEventListener('wheel', function (e) {
+  if (e.ctrlKey) {
+    schedAnimVarChange(x => backlight.angle += x, () => clampDelta(backlight.angle, e.deltaY > 0 ?  -Math.PI / 56 : Math.PI / 56, Math.PI/6, Math.PI/2), 25, () => backlight.angle = clamp(backlight.angle, Math.PI/6, Math.PI/2));
+    e.preventDefault();
+  } else schedAnimVarChange(x => backlight.intensity += x, () => clampDelta(backlight.intensity, e.deltaY > 0 ? -0.15 : 0.15, 1, 3), 25, () => backlight.intensity = clamp(backlight.intensity, 1, 3));
+}, {passive: false});
+
 function start() {
   AOS.init({once: true});
   started = true;
   clock.start();
 }
 
+function clampDelta(x, delta, min, max) {
+  var clamped = clamp(x+delta, min, max);
+  return delta < 0 && clamped > x+delta || clamped < x+delta ? clamped - x : delta;
+}
+
+function clamp(x, min, max) {
+  return Math.max(Math.min(x, max), min);
+}
+
+var ongoingVarAnim = false;
+var varAnimQueue = [];
+
+function schedAnimVarChange(adder, deltaGetter, time, onready) {
+  if (!ongoingVarAnim) animVarChange(adder, deltaGetter, time, onready);
+  else varAnimQueue.push({adder: adder, deltaGetter: deltaGetter, time: time, onready: onready});
+}
+
+function animVarChange(adder, deltaGetter, time, onready) {
+  var delta = deltaGetter();
+  if (delta == 0 || Math.abs(delta) < 0.1e-4) {
+    if (varAnimQueue.length) {
+        var entry = varAnimQueue.shift();
+        animVarChange(entry.adder, entry.deltaGetter, entry.time, entry.onready);
+      }
+    return;
+  }
+  ongoingVarAnim = true;
+  var step = delta / time;
+  var total = 0;
+  var start = adder(0);
+  var end = start+delta;
+  function add() {
+    adder(step);
+    total += step;
+    if (Math.abs(total) < Math.abs(delta)) setTimeout(add, 1);
+    else {
+      adder(end-adder(0)); // Float-precision may mess with accuracy of end product, so we fix that here.
+      if (onready) onready();
+      ongoingVarAnim = false;
+      if (varAnimQueue.length) {
+        var entry = varAnimQueue.shift();
+        animVarChange(entry.adder, entry.deltaGetter, entry.time, entry.onready);
+      }
+    }
+  }
+  add();
+}
+
 function setup() {
   renderer.width = renderer.domElement.width = window.innerWidth;
   renderer.height = renderer.domElement.height = window.innerHeight;
   camera.aspect = renderer.width / renderer.height;
-  let dist = camera.position.z - flagMesh.position.z;
-  let height = 3;
-  camera.fov = 2 * Math.atan(height / (2 * dist)) * (180 / Math.PI);
+  camera.fov = 2 * Math.atan(3 / (2 * (camera.position.z - flagMesh.position.z))) * (180 / Math.PI);
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
+  centerX = window.innerWidth / 2;
+  centerY = window.innerHeight / 2;
+  if (!backlight.parent) {
+    var intensity = backlight.intensity;
+    backlight.intensity = 0;
+    scene.add(backlight);
+    animVarChange(x => backlight.intensity += x, () => intensity, 250);
+  }
 }
 setup();
 
